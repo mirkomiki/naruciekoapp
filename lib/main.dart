93c26901372200b6f070a4d3cc0abfbb3f9ebfc4
Viktor26903 <133779282+Viktor26903@users.noreply.github.com>
2024-02-24 12:15:38 +0100

    maknut komentar proba

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,6 @@ class MyApp extends StatelessWidget {
 
       initialData: UserModel(uid: 'X'), // DULJINA 11 VELIKO X
       child: const MaterialApp(
-        // proba123
         home: Wrapper(),
       ),
     );
