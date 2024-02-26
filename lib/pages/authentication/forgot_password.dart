161a09e7c22b306c0de5c4b1d37ace2a644a9804
l-bu <buriclucija38@gmail.com>
2024-02-26 19:46:03 +0100

    forgot password gotov
    dela, ali je ružno
    dona popravljaj

--- lib/pages/authentication/forgot_password.dart
+++ lib/pages/authentication/forgot_password.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:naruciekoapp/datatype/customTextField.dart';
 
 class ForgotPasswordPage extends StatefulWidget {
   const ForgotPasswordPage({Key? key}) : super(key: key);
@@ -9,6 +11,37 @@ class ForgotPasswordPage extends StatefulWidget {
 }
 
 class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
+  final _emailController = TextEditingController();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    super.dispose();
+  }
+
+  Future passwordReset() async {
+    try {
+      await FirebaseAuth.instance
+          .sendPasswordResetEmail(email: _emailController.text.trim());
+      showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              content: Text("Link za ponovno postavljanje lozinke je poslan"),
+            );
+          });
+    } on FirebaseAuthException catch (e) {
+      print(e);
+      showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              content: Text("Neispravna e-mail adresa"),
+            );
+          });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,7 +51,18 @@ class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
         ),
         body: Column(children: [
           Text(
-              "Unesite Vašu e-mail adresu na koju ćemo Vam poslati link za ponovno postavljanje lozinke")
+            "Unesite e-mail adresu na koju ćemo Vam poslati link za ponovno postavljanje lozinke:",
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 20),
+          ),
+          CustomTextField(
+              controller: _emailController,
+              hintText: 'Email',
+              obsureText: false),
+          MaterialButton(
+            onPressed: passwordReset,
+            child: Text("Ponovo postavi"),
+          )
         ]));
   }
 }
