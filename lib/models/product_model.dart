2099048cb78b1535d07b89444fa97dccee826d2e
l-bu <buriclucija38@gmail.com>
2024-02-27 00:44:52 +0100

    models update

--- lib/models/product_model.dart
+++ lib/models/product_model.dart
@@ -1,1 +1,47 @@
-class ProductModel {}
+enum Categories { food, drink, other }
+
+enum Unit { kilogram, piece }
+
+class ProductModel {
+  final String id;
+  String? name;
+  String photo = "";
+  String description = "";
+  double? price;
+  Unit? unit;
+  double? quantity;
+  double rating = 0;
+  int _ratingSum = 0;
+  int _numberOfRatings = 0;
+  Categories? category;
+
+  ProductModel({required this.id});
+
+  ProductModel.full(
+      {required this.id,
+      this.name,
+      this.price,
+      this.unit,
+      this.quantity,
+      this.category});
+
+  void setName(String name) => this.name = name;
+
+  void setPhoto(String photo) => this.photo = photo;
+
+  void setDescription(String description) => this.description = description;
+
+  void setPrice(double price) => this.price = price;
+
+  void setUnit(Unit unit) => this.unit = unit;
+
+  void setQuantity(double quantity) => this.quantity = quantity;
+
+  void setCategory(Categories category) => this.category = category;
+
+  void addRating(int rating) {
+    _numberOfRatings++;
+    _ratingSum += rating;
+    this.rating = _ratingSum / _numberOfRatings;
+  }
+}
