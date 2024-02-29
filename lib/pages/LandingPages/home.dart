2173daaefd3e1e742c8a767e7e1e342d1e5d842e
donaweiner <126697557+donaweiner@users.noreply.github.com>
2024-02-29 05:30:59 +0100

    uljepasn homepage - items, sign out

--- lib/pages/LandingPages/home.dart
+++ lib/pages/LandingPages/home.dart
@@ -1,10 +1,12 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:naruciekoapp/pages/LandingPages/pages.dart';
 import 'package:naruciekoapp/globalData.dart';
 import 'package:naruciekoapp/services/database.dart';
 
 class Home extends StatefulWidget {
-  const Home({super.key});
+  const Home({Key? key}) : super(key: key);
 
   @override
   State<Home> createState() => _HomeState();
@@ -16,10 +18,48 @@ class _HomeState extends State<Home> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Naruči naše'),
-        backgroundColor: const Color.fromARGB(255, 21, 86, 55),
+        backgroundColor: Color.fromARGB(255, 26, 102, 65),
+        actions: [
+          IconButton(onPressed: () async {
+            await FirebaseAuth.instance.signOut();
+          }
+          , icon: const Icon(Icons.exit_to_app))
+        ]
       ),
-      body: const Padding(
-        padding: EdgeInsets.all(doubleGapBetweenContainers),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: GridView.count(
+          crossAxisCount: 2, // 2 columns
+          mainAxisSpacing: 8.0,
+          crossAxisSpacing: 8.0,
+          children: [
+            _buildCategoryButton(context, "Voće", Icons.local_florist, () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => FruitPage()),
+              );
+            }),
+            _buildCategoryButton(context, "Povrće", Icons.local_florist, () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => VegetablesPage()),
+              );
+            }),
+            _buildCategoryButton(
+                context, "Meso i mliječni proizvodi", Icons.local_florist, () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => MeatAndDairyPage()),
+              );
+            }),
+            _buildCategoryButton(context, "Ostalo", Icons.local_florist, () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => OtherPage()),
+              );
+            }),
+          ],
+        ),
       ),
       floatingActionButton: ElevatedButton(
         onPressed: () {
@@ -34,4 +74,195 @@ class _HomeState extends State<Home> {
       ),
     );
   }
+
+  Widget _buildCategoryButton(BuildContext context, String title, IconData icon, VoidCallback onPressed) {
+    return ElevatedButton.icon(
+      onPressed: onPressed,
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all<Color>(Colors.lightGreen), // Change button color to light green
+        foregroundColor: MaterialStateProperty.all<Color>(Colors.black), // Change text color to black
+        padding: MaterialStateProperty.all<EdgeInsetsGeometry>(EdgeInsets.all(8)), // Adjust padding
+      ),
+      icon: Icon(icon), // Add icon
+      label: Text(
+        title,
+        style: TextStyle(fontSize: 16),
+      ),
+    );
+  }
 }
+
+
+class FruitPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Voće'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'Pretraži voće...',
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 20), // Add spacing between search bar and cards
+          _buildFruitCard(
+            context,
+            'Jabuke',
+            'assets/blank-profile-image.jpg', // Example relative path to the image file
+          ),
+          SizedBox(height: 20), // Add spacing between cards
+          _buildFruitCard(
+            context,
+            'Naranče',
+            'assets/blank-profile-image.jpg', // Example relative path to the image file
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildFruitCard(BuildContext context, String name, String imagePath) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => AppleSellersPage()), // Navigate to AppleSellersPage
+        );
+      },
+      child: Card(
+        margin: EdgeInsets.all(16),
+        child: ListTile(
+          leading: Image.asset(
+            imagePath,
+            width: 100, // Adjust image width as needed
+            height: 100, // Adjust image height as needed
+            fit: BoxFit.cover,
+          ),
+          title: Text(name),
+        ),
+      ),
+    );
+  }
+}
+
+
+class AppleSellersPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sellers of Jabuke'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          _buildAppleSellerCard('OPG Weiner', 'Spickovina', '5 € / kg', 'assets/blank-profile-image.jpg', 5),
+          _buildAppleSellerCard('OPG Mikro Bušilica', 'Babina Guzica', '6 € / kg', 'assets/blank-profile-image.jpg', 2),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildAppleSellerCard(String opgName, String village, String price, String imagePath, int rating) {
+    return Card(
+      margin: EdgeInsets.all(16),
+      child: ListTile(
+        leading: Image.asset(
+          imagePath,
+          width: 100, // Adjust image width as needed
+          height: 100, // Adjust image height as needed
+          fit: BoxFit.cover,
+        ),
+        title: Text(opgName),
+        subtitle: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(village), // Display village
+            Text(price),
+            SizedBox(height: 5), // Add spacing between village and rating
+            Row(
+              children: [
+                Icon(Icons.star, color: const Color.fromARGB(255, 255, 218, 7), size: 20), // Star icon for rating
+                SizedBox(width: 5),
+                Text('$rating / 5'), // Display rating
+              ],
+            ),
+          ],
+        ),
+        trailing: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            IconButton(
+              onPressed: () {
+                // Handle decrease quantity
+              },
+              icon: Icon(Icons.remove),
+            ),
+            Text('0'), // Display quantity (non-functional)
+            IconButton(
+              onPressed: () {
+                // Handle increase quantity
+              },
+              icon: Icon(Icons.add),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class VegetablesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Povrće'),
+      ),
+      body: Center(
+        child: Text('Stranica s povrćem'),
+      ),
+    );
+  }
+}
+
+class MeatAndDairyPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Meso i mliječni proizvodi'),
+      ),
+      body: Center(
+        child: Text('Stranica s mesom i mliječnim proizvodima'),
+      ),
+    );
+  }
+}
+
+class OtherPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Ostalo'),
+      ),
+      body: Center(
+        child: Text('Stranica s ostalim proizvodima'),
+      ),
+    );
+  }
+}
+
+
